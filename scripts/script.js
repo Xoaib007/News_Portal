e8635e9ce758542fc53984f1141c80e3d62be121
Sh0a!b77 <shoaib.rehman.2607@gmail.com>
2022-09-04 01:54:09 +0200

    news added

--- scripts/script.js
+++ scripts/script.js
@@ -2,30 +2,68 @@ const loadCategories = () =>{
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then( res => res.json())
     .then( data => displayeCategories(data.data.news_category))
-    // .then( data => console.log(data.data.category_id))
 };
 
 const displayeCategories = categories =>{
-    console.log(categories);
     const categoryContainer = document.getElementById('category-list');
     categories.forEach(categoryName => {
         const categoryDiv = document.createElement('div');
-        categoryDiv.classList.add('category');
+        categoryDiv.classList.add('category1');
         categoryDiv.innerHTML = `
-        <button onclick="displayNews(${categoryName.category_id})" class="category">${categoryName.category_name}</button>
+        <button onclick="loadNews(${categoryName.category_id})" class="category">${categoryName.category_name}</button>
         `;
         categoryContainer.appendChild(categoryDiv);
     });
 };
-const displayNews = (catId) => {
+
+const loadNews = (catId) => {
     console.log('id-' , catId)
     const url = `https://openapi.programming-hero.com/api/news/category/0${catId}`;
     console.log(url)
 
     fetch (url)
     .then( res => res.json())
-    .then( data => console.log(data))
+    .then( data => displayNews(data.data))
+};
 
+const displayNews = allNews =>{
+    console.log(allNews)
+    const newsContainer = document.getElementById('all-news');
+    allNews.forEach( news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('newses');
+        newsDiv.innerHTML = `
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${news.title}</h5>
+                            <p class="card-text">${news.details}</p>
+                            <section>
+                                <div class="d-flex">
+                                    <img src="${news.author.img}" class="author-image" alt="">
+                                    <div >
+                                        <p>${news.author.name}</p>
+                                        <p>${news.author.published_date}</p>
+                                    </div>
+                                    <div>
+                                        <i class="fa-solid fa-eye"></i>
+                                        <p>${news.total_view}</p>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        newsContainer.appendChild(newsDiv);
+    })
 }
 
 loadCategories();
+
+loadNews();
